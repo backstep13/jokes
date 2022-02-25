9114ac656d8dea30bdc9896266d627a1c1840e08
backstep13 <backstep13@tut.by>
2022-02-26 00:06:48 +0300

    app in Docker

--- aphorisms/main.py
+++ aphorisms/main.py
@@ -21,3 +21,6 @@ def get_item():
 	with open(file, 'r', encoding='utf-8') as f:
 		data = json.load(f)
 	return jsonify(data['texts'][num]['text'])
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')
