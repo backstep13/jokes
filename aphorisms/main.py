d5c756c2835201ef472cca784f0cf589ccd3ee8f
backstep13 <backstep13@tut.by>
2022-04-16 23:29:11 +0300

    create db, add SQlAlchemy and RESTFul to main file

--- aphorisms/main.py
+++ aphorisms/main.py
@@ -1,26 +1,36 @@
-from flask import Flask, render_template, jsonify
-from pathlib import Path
-import os
-import json
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api
 import random
 
 
-app_dir = Path(__file__).resolve().parent  # project path
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///aphorisms.db'
+db = SQLAlchemy(app)
+api = Api(app)
+
+
+class Aphorism(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	text = db.Column(db.Text, nullable=False)
+
 
 @app.route("/")
 def index():
 	"""Function return HTML of main page"""
 	return render_template('index.html')
-	
-@app.route("/api/")
-def get_item():
-	"""Function return JSON answer from storage to client """
-	num = random.randint(0, 16)
-	file = os.path.join(app_dir, 'storage.json')
-	with open(file, 'r', encoding='utf-8') as f:
-		data = json.load(f)
-	return jsonify(data['texts'][num]['text'])
+
+
+class Item(Resource):
+	def get(self):
+		"""Function return JSON answer from db to client """
+		num = random.randint(0, 16)
+		data = Aphorism.query.get(num)
+		return data.text
+
+
+api.add_resource(Item, '/api')
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0', debug=True)
